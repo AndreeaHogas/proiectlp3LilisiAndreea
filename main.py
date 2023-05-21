87500cf67b32a7064950ebca368b0051176816a7
AndreeaHogas <129149260+AndreeaHogas@users.noreply.github.com>
2023-05-21 21:54:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -70,20 +70,20 @@ while current_page <= 13:
                 cartii.append(book)
 # se afișează mesajul pentru verificarea paginii curente
     print(f"Verifying page: {current_page}")
- # se incrementează numărul paginii curente
+# se incrementează numărul paginii curente
     current_page += 1
- # se adaugă o pauză de 1 secundă între cereri pentru a nu suprasolicita serverul
+# se adaugă o pauză de 1 secundă între cereri pentru a nu suprasolicita serverul
     time.sleep(1) 
 # se găsește linkul către pagina următoare
     next_page_link = soup.find("a", title="La pagina următoare")
-  # dacă nu există link către pagina următoare, se încheie bucla
+# dacă nu există link către pagina următoare, se încheie bucla
     if next_page_link is None:
         break
-  # se extrage URL-ul paginii următoare
+# se extrage URL-ul paginii următoare
     next_page_url = next_page_link["href"]
     # Extragem numărul paginii următoare din URL
     current_page = int(re.search(r"PageNumber=(\d+)", next_page_url).group(1))
-  # se salvează informațiile despre cărți într-un fișier JSON
+# se salvează informațiile despre cărți într-un fișier JSON
 with open("star_wars_cartii.json", "w+", encoding="utf8") as json_file:
     json.dump({"cartii": cartii}, json_file, indent=4)
 
